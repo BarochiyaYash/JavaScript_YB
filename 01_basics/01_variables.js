a4e5e5071287a33021c30afa1619d3ccacc5f4e7
Yash Barochiya <yashbarochia2002@gmail.com>
2024-12-22 15:08:35 +0530

    Start JS

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+console.log("I am from | 01_variables");
+
+const accountId = 100;
+var emailID = "yash@gmail.com";
+let emailPassword = "100yash";
+
+// -- Const Veriable can't chage they value
+// accountId = 101;
+
+console.log(accountId);
+console.log(emailID);
+console.log(emailPassword);
